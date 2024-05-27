7848912d5140e6d68829e56b0384b8a47d73309c
doyoung0627 <165134940+doyoung0627@users.noreply.github.com>
2024-05-27 19:53:57 +0900

    Add files via upload

--- testapp/app/src/main/java/com/example/testapp/MainActivity.kt
+++ testapp/app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,20 +1,33 @@
-package com.example.testapp
+package com.example.yourapp
 
+import android.R
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.swproject.FoodRefrigeratorActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_comment)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main)
+
+        val textView1 = findViewById<TextView>(R.id.rfbpsac7wztp)
+        textView1.setOnClickListener {
+            val intent = Intent(
+                this@MainActivity,
+                FoodRefrigeratorActivity::class.java
+            )
+            startActivity(intent)
+        }
+
+        val textView2 = findViewById<TextView>(R.id.rd4p0s5ybnm)
+        textView2.setOnClickListener {
+            val intent = Intent(
+                this@MainActivity,
+                FoodSearchActivity::class.java
+            )
+            startActivity(intent)
         }
     }
 }
