f0889a034f07c909e2b38ee9a47f0126499549a1
circleolami <circleolami@gmail.com>
2024-06-07 13:51:40 +0900

    Update App.js

--- zzipfood/frontend/src/App.js
+++ zzipfood/frontend/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FoodSearchPage from "./pages/FoodSearchPage";
+import FridgePage from "./pages/FridgePage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">메인 페이지</Link>
+            </li>
+            <li>
+              <Link to="/food-search">먹고싶은 음식</Link>
+            </li>
+            <li>
+              <Link to="/fridge">냉장고 재료 확인</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/food-search" element={<FoodSearchPage />} />
+          <Route path="/fridge" element={<FridgePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
