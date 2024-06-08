b92e17eedb5d8dd333feb1964362bd6236a63060
circleolami <circleolami@gmail.com>
2024-06-08 19:38:21 +0900

    make food.cs

---
+++ MyApiProject/Models/Food.cs
@@ -0,0 +1,10 @@
+namespace MyApiProject.Models
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public float Amount { get; set; }
+        public string Unit { get; set; }
+        public float Price { get; set; }
+    }
+}
