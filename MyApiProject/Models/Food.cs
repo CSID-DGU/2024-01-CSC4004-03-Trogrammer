857f236d20c468e2d86d371508f4733e0e08e0a0
circleolami <circleolami@gmail.com>
2024-06-08 19:48:50 +0900

    update food.cs

--- MyApiProject/Models/Food.cs
+++ MyApiProject/Models/Food.cs
@@ -6,5 +6,11 @@ namespace MyApiProject.Models
         public float Amount { get; set; }
         public string Unit { get; set; }
         public float Price { get; set; }
+
+        public Food()
+        {
+            Name = string.Empty;
+            Unit = string.Empty;
+        }
     }
 }
