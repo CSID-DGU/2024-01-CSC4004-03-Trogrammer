b9b0802e0206bbc6328bb930f47b5e9f8102b8fc
circleolami <circleolami@gmail.com>
2024-06-08 20:03:06 +0900

    update recipe

--- Assets/Database.cs
+++ Assets/Database.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-
 public static class Database {
 
     public enum Unit {
@@ -83,10 +81,12 @@ public static class Database {
         public string name;
         public RecipeType type;
         public List<Foodset> foodset;
-        public Recipe(string name, RecipeType type, List<Foodset> foodset) {
+        public List<string> steps; // 추가된 부분
+        public Recipe(string name, RecipeType type, List<Foodset> foodset, List<string> steps) { // 변경된 부분
             this.name = name;
             this.type = type;
             this.foodset = foodset;
+            this.steps = steps; // 추가된 부분
         }
     }
 
@@ -95,11 +95,27 @@ public static class Database {
             new Foodset("김치", 100.0f),
             new Foodset("대파",   1.0f),
             new Foodset("두부",   1.0f),
+        },
+        new List<string> { // 김치찌개 요리 과정
+            "1. 김치를 적당한 크기로 자릅니다.",
+            "2. 대파는 어슷썰기합니다.",
+            "3. 두부는 먹기 좋은 크기로 자릅니다.",
+            "4. 냄비에 물을 붓고 김치를 넣고 끓입니다.",
+            "5. 김치가 어느 정도 익으면 대파를 넣고 끓입니다.",
+            "6. 마지막으로 두부를 넣고 한 번 더 끓입니다.",
+            "7. 모든 재료가 잘 익으면 김치찌개를 그릇에 담아냅니다."
         }),
         new Recipe("계란말이", RecipeType.Korean, new List<Foodset>{
             new Foodset("계란",   4.0f),
             new Foodset("대파",   1.0f),
             new Foodset("소금",   1.0f),
+        },
+        new List<string> {
+            "1. 계란을 풀어 소금으로 간을 맞춥니다.",
+            "2. 대파를 잘게 썰어 계란물에 섞습니다.",
+            "3. 팬에 기름을 두르고 계란물을 얇게 부칩니다.",
+            "4. 계란이 익기 시작하면 말아줍니다.",
+            "5. 완성된 계란말이를 적당한 크기로 자릅니다."
         }),
 
         new Recipe("된장찌개", RecipeType.Korean, new List<Foodset>{
@@ -108,6 +124,15 @@ public static class Database {
             new Foodset("양파", 0.5f),
             new Foodset("호박", 100.0f),
             new Foodset("고추", 10.0f),
+        },
+        new List<string> { // 된장찌개 요리 과정
+            "1. 감자와 호박을 적당한 크기로 자릅니다.",
+            "2. 양파는 얇게 슬라이스합니다.",
+            "3. 고추는 얇게 썰어 준비합니다.",
+            "4. 냄비에 물을 붓고 된장을 풀어줍니다.",
+            "5. 된장이 풀린 물에 감자와 호박을 넣고 끓입니다.",
+            "6. 감자가 익으면 양파와 고추를 넣고 다시 끓입니다.",
+            "7. 모든 재료가 잘 익으면 된장찌개를 그릇에 담아냅니다."
         }),
 
         new Recipe("크림파스타", RecipeType.Western, new List<Foodset>{
@@ -115,6 +140,14 @@ public static class Database {
             new Foodset("크림소스", 150.0f),
             new Foodset("버섯", 50.0f),
             new Foodset("치즈", 30.0f),
+        },
+        new List<string> {
+            "1. 파스타면을 끓는 물에 삶아줍니다.",
+            "2. 팬에 크림소스를 붓고 데웁니다.",
+            "3. 버섯을 썰어 팬에 넣고 익힙니다.",
+            "4. 삶은 파스타면을 팬에 넣고 소스와 섞어줍니다.",
+            "5. 치즈를 넣고 잘 섞어줍니다.",
+            "6. 완성된 크림파스타를 접시에 담아냅니다."
         }),
 
         new Recipe("짜장면", RecipeType.Chinese, new List<Foodset>{
@@ -122,70 +155,172 @@ public static class Database {
             new Foodset("돼지고기", 100.0f),
             new Foodset("양파", 1.0f),
             new Foodset("짜장면", 200.0f),
+        },
+        new List<string> {
+            "1. 돼지고기를 적당한 크기로 자릅니다.",
+            "2. 양파를 썰어줍니다.",
+            "3. 팬에 기름을 두르고 돼지고기와 양파를 볶습니다.",
+            "4. 짜장소스를 팬에 붓고 함께 끓입니다.",
+            "5. 짜장면을 끓는 물에 삶아줍니다.",
+            "6. 삶은 짜장면을 접시에 담고 소스를 부어줍니다."
         }),
 
         new Recipe("불고기", RecipeType.Korean, new List<Foodset>{
             new Foodset("소고기", 200.0f),
             new Foodset("양파", 1.0f),
             new Foodset("간장", 20.0f),
+        },
+        new List<string> {
+            "1. 소고기를 얇게 썰어줍니다.",
+            "2. 양파를 썰어줍니다.",
+            "3. 팬에 소고기와 양파를 넣고 볶습니다.",
+            "4. 간장을 넣고 잘 섞어줍니다.",
+            "5. 소고기가 익으면 불고기를 접시에 담아냅니다."
         }),
+
         new Recipe("삼계탕", RecipeType.Korean, new List<Foodset>{
             new Foodset("닭고기", 300.0f),
             new Foodset("대파", 1.0f),
             new Foodset("소금", 5.0f),
+        },
+        new List<string> {
+            "1. 닭고기를 깨끗이 씻습니다.",
+            "2. 냄비에 물을 붓고 닭고기를 넣고 끓입니다.",
+            "3. 대파를 썰어 냄비에 넣습니다.",
+            "4. 소금으로 간을 맞춥니다.",
+            "5. 닭고기가 익으면 삼계탕을 그릇에 담아냅니다."
         }),
+
         new Recipe("비빔밥", RecipeType.Korean, new List<Foodset>{
             new Foodset("밥", 200.0f),
             new Foodset("김치", 50.0f),
             new Foodset("계란", 1.0f),
+        },
+        new List<string> {
+            "1. 밥을 그릇에 담습니다.",
+            "2. 김치를 적당한 크기로 자릅니다.",
+            "3. 계란을 프라이팬에 부쳐줍니다.",
+            "4. 밥 위에 김치와 계란을 올립니다.",
+            "5. 고추장이나 다른 양념을 넣고 비벼줍니다."
         }),
 
         new Recipe("탕수육", RecipeType.Chinese, new List<Foodset>{
             new Foodset("돼지고기", 200.0f),
             new Foodset("양파", 1.0f),
             new Foodset("간장", 30.0f),
+        },
+        new List<string> {
+            "1. 돼지고기를 적당한 크기로 자릅니다.",
+            "2. 양파를 썰어줍니다.",
+            "3. 돼지고기에 전분을 묻혀줍니다.",
+            "4. 팬에 기름을 두르고 돼지고기를 튀깁니다.",
+            "5. 소스를 만들어 돼지고기와 양파를 넣고 볶습니다.",
+            "6. 완성된 탕수육을 접시에 담아냅니다."
         }),
+
         new Recipe("짬뽕", RecipeType.Chinese, new List<Foodset>{
             new Foodset("돼지고기", 150.0f),
             new Foodset("고추", 20.0f),
             new Foodset("양파", 1.0f),
+        },
+        new List<string> {
+            "1. 돼지고기를 적당한 크기로 자릅니다.",
+            "2. 양파를 썰어줍니다.",
+            "3. 고추를 썰어줍니다.",
+            "4. 팬에 기름을 두르고 돼지고기와 양파, 고추를 볶습니다.",
+            "5. 물을 넣고 끓입니다.",
+            "6. 면을 삶아 짬뽕 국물에 넣습니다.",
+            "7. 완성된 짬뽕을 그릇에 담아냅니다."
         }),
+
         new Recipe("마파두부", RecipeType.Chinese, new List<Foodset>{
             new Foodset("두부", 200.0f),
             new Foodset("돼지고기", 100.0f),
             new Foodset("양파", 1.0f),
+        },
+        new List<string> {
+            "1. 두부를 적당한 크기로 자릅니다.",
+            "2. 돼지고기를 적당한 크기로 자릅니다.",
+            "3. 양파를 썰어줍니다.",
+            "4. 팬에 기름을 두르고 돼지고기와 양파를 볶습니다.",
+            "5. 두부를 넣고 함께 볶습니다.",
+            "6. 소스를 넣고 잘 섞어줍니다.",
+            "7. 완성된 마파두부를 접시에 담아냅니다."
         }),
 
         new Recipe("초밥", RecipeType.Japanese, new List<Foodset>{
             new Foodset("밥", 200.0f),
             new Foodset("생선", 100.0f),
             new Foodset("간장", 10.0f),
+        },
+        new List<string> {
+            "1. 밥을 준비합니다.",
+            "2. 생선을 얇게 썰어줍니다.",
+            "3. 밥을 손으로 뭉쳐 생선을 올립니다.",
+            "4. 간장을 곁들여서 제공합니다."
         }),
+
         new Recipe("라멘", RecipeType.Japanese, new List<Foodset>{
             new Foodset("돼지고기", 100.0f),
             new Foodset("파스타면", 200.0f),
             new Foodset("간장", 20.0f),
+        },
+        new List<string> {
+            "1. 돼지고기를 적당한 크기로 자릅니다.",
+            "2. 파스타면을 끓는 물에 삶아줍니다.",
+            "3. 팬에 돼지고기를 볶습니다.",
+            "4. 면을 국물에 넣고 돼지고기를 올립니다.",
+            "5. 간장으로 간을 맞춥니다.",
+            "6. 완성된 라멘을 그릇에 담아냅니다."
         }),
+
         new Recipe("돈까스", RecipeType.Japanese, new List<Foodset>{ 
             new Foodset("돼지 등심", 200.0f),
             new Foodset("소금", 5.0f),
             new Foodset("계란", 1.0f),
+        },
+        new List<string> {
+            "1. 돼지 등심에 소금과 후추로 간을 합니다.",
+            "2. 계란을 풀어 돼지 등심에 묻힙니다.",
+            "3. 빵가루를 묻혀줍니다.",
+            "4. 팬에 기름을 두르고 돼지 등심을 튀깁니다.",
+            "5. 완성된 돈까스를 적당한 크기로 자릅니다."
         }),
 
         new Recipe("스테이크", RecipeType.Western, new List<Foodset>{
             new Foodset("소고기", 300.0f),
             new Foodset("소금", 5.0f),
             new Foodset("버섯", 50.0f),
+        },
+        new List<string> {
+            "1. 소고기에 소금과 후추로 간을 합니다.",
+            "2. 팬에 기름을 두르고 소고기를 구워줍니다.",
+            "3. 버섯을 썰어 함께 구워줍니다.",
+            "4. 완성된 스테이크를 접시에 담아냅니다."
         }),
+
         new Recipe("샐러드", RecipeType.Western, new List<Foodset>{
             new Foodset("오이", 50.0f),
             new Foodset("양파", 1.0f),
             new Foodset("소금", 5.0f),
+        },
+        new List<string> {
+            "1. 오이를 얇게 썰어줍니다.",
+            "2. 양파를 얇게 썰어줍니다.",
+            "3. 모든 재료를 볼에 넣고 소금을 뿌려줍니다.",
+            "4. 잘 섞어줍니다."
         }),
+
         new Recipe("피자", RecipeType.Western, new List<Foodset>{
             new Foodset("치즈", 100.0f),
             new Foodset("버섯", 50.0f),
             new Foodset("피자도우", 300.0f),
+        },
+        new List<string> {
+            "1. 피자도우를 준비합니다.",
+            "2. 치즈와 버섯을 올립니다.",
+            "3. 오븐에 구워줍니다.",
+            "4. 완성된 피자를 적당한 크기로 자릅니다."
         }),
     };
 
