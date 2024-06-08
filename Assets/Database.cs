9c328b76684bb842d06d7a038496100199b93dee
circleolami <circleolami@gmail.com>
2024-06-08 18:51:45 +0900

    음식 카테고리 분류

--- Assets/Database.cs
+++ Assets/Database.cs
@@ -125,7 +125,7 @@ public static class Database {
         }),
 
         new Recipe("불고기", RecipeType.Korean, new List<Foodset>{
-            new Foodset("쇠고기", 200.0f),
+            new Foodset("소고기", 200.0f),
             new Foodset("양파", 1.0f),
             new Foodset("간장", 20.0f),
         }),
@@ -173,7 +173,7 @@ public static class Database {
         }),
 
         new Recipe("스테이크", RecipeType.Western, new List<Foodset>{
-            new Foodset("쇠고기", 300.0f),
+            new Foodset("소고기", 300.0f),
             new Foodset("소금", 5.0f),
             new Foodset("버섯", 50.0f),
         }),
@@ -192,9 +192,11 @@ public static class Database {
     public struct Menuset {
         public Recipe recipe;
         public float price;
-        public Menuset(string name, float price) {
+        public string category;
+        public Menuset(string name, float price, string category) {
             this.recipe = Database.recipe.Find(x => x.name == name);
             this.price = price;
+            this.category = category;
         }
     }
 
@@ -211,61 +213,61 @@ public static class Database {
 
     public readonly static List<Restaurant> restaurant = new List<Restaurant>{
         new Restaurant("김치만선생 동대필동점", new List<Menuset>{
-            new Menuset("김치찌개", 7000.0f),
-            new Menuset("계란말이", 6000.0f),
+            new Menuset("김치찌개", 7000.0f, "김치찌개"),
+            new Menuset("계란말이", 6000.0f, "계란말이"),
         }),
 
         new Restaurant("황토골", new List<Menuset>{
-			new Menuset("김치찌개", 8000.0f),
-            new Menuset("된장찌개", 8000.0f),
+            new Menuset("김치찌개", 8000.0f, "김치찌개"),
+            new Menuset("된장찌개", 8000.0f, "된장찌개"),
         }),
 
         new Restaurant("필동함박 충무로 본점", new List<Menuset>{
-            new Menuset("까르보파스타", 11500.0f),
-			new Menuset("투움바파스타", 11500.0f),
+            new Menuset("까르보파스타", 11500.0f, "크림파스타"),
+            new Menuset("투움바파스타", 11500.0f, "크림파스타"),
         }),
         new Restaurant("홍짜장 동국대점", new List<Menuset>{
-            new Menuset("짜장면", 5500.0f),
-			new Menuset("짬뽕", 6500.0f),
+            new Menuset("짜장면", 5500.0f, "짜장면"),
+            new Menuset("짬뽕", 6500.0f, "짬뽕"),
         }),
 
         new Restaurant("평강삼계탕 장충점", new List<Menuset>{
-            new Menuset("삼계탕", 17000.0f),
+            new Menuset("삼계탕", 17000.0f, "삼계탕"),
         }),
-		new Restaurant("일미정", new List<Menuset>{
-            new Menuset("불고기", 8000.0f),
-			new Menuset("된장찌개", 9000.0f),
+        new Restaurant("일미정", new List<Menuset>{
+            new Menuset("불고기", 8000.0f, "불고기"),
+            new Menuset("된장찌개", 9000.0f, "된장찌개"),
         }),
         new Restaurant("필동반점", new List<Menuset>{
-            new Menuset("짬뽕", 7000.0f),
-            new Menuset("짜장면", 6000.0f),
+            new Menuset("짬뽕", 7000.0f, "짬뽕"),
+            new Menuset("짜장면", 6000.0f, "짜장면"),
         }),
         new Restaurant("스시노칸도 충무로역점", new List<Menuset>{
-            new Menuset("초밥", 22000.0f),
+            new Menuset("초밥", 22000.0f, "초밥"),
         }),
-		new Restaurant("스담", new List<Menuset>{
-            new Menuset("초밥", 18000.0f),
+        new Restaurant("스담", new List<Menuset>{
+            new Menuset("초밥", 18000.0f, "초밥"),
         }),
-		new Restaurant("낙원의소바", new List<Menuset>{
-            new Menuset("돈까스", 15000.0f),
-			new Menuset("로스카츠", 13000.0f),
+        new Restaurant("낙원의소바", new List<Menuset>{
+            new Menuset("돈까스", 15000.0f, "돈까스"),
+            new Menuset("로스카츠", 13000.0f, "돈까스"),
         }),
-		new Restaurant("서울카츠 충무로본점", new List<Menuset>{
-            new Menuset("등심 카츠", 14000.0f),
-			new Menuset("안심 카츠", 15000.0f),
+        new Restaurant("서울카츠 충무로본점", new List<Menuset>{
+            new Menuset("등심 카츠", 14000.0f, "돈까스"),
+            new Menuset("안심 카츠", 15000.0f, "돈까스"),
         }),
-		new Restaurant("모리짱", new List<Menuset>{
-            new Menuset("돈고츠라멘", 8000.0f),
-			new Menuset("소유라멘", 7000.0f),
+        new Restaurant("모리짱", new List<Menuset>{
+            new Menuset("돈코츠라멘", 8000.0f, "라멘"),
+            new Menuset("소유라멘", 7000.0f, "라멘"),
         }),
         new Restaurant("도치피자 장충점", new List<Menuset>{
-            new Menuset("디아볼라 피자", 24000.0f),
-            new Menuset("고르곤졸라 피자",22000.0f),
+            new Menuset("디아볼라 피자", 24000.0f, "피자"),
+            new Menuset("고르곤졸라 피자",22000.0f, "피자"),
         }),
-		new Restaurant("미스터피자 충무로점", new List<Menuset>{
-            new Menuset("포테이토골드 피자", 31500.0f),
-            new Menuset("페퍼로니 피자",20500.0f),
-			new Menuset("콤비네이션 피자",21500.0f),
+        new Restaurant("미스터피자 충무로점", new List<Menuset>{
+            new Menuset("포테이토골드 피자", 31500.0f, "피자"),
+            new Menuset("페퍼로니 피자",20500.0f, "피자"),
+            new Menuset("콤비네이션 피자",21500.0f, "피자"),
         }),
     };
 }
